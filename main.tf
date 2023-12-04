fd32aafd25185c8fe427d89cda8a81fe2fe7ddd0
Harikesha Suresh <harikeshasuresh@gmail.com>
2023-12-04 10:12:33 +1000

    The instance installs and runs the containers, some issues accessing the servers when using the AWS public DNS for the instance. (#1)

--- main.tf
+++ main.tf
@@ -61,55 +61,16 @@ resource "aws_security_group" "ticket_overflow_security_group" {
   }
 }
 
-# resource "aws_security_group" "ticketoverflow_security" {
-#   name        = "allow_ssh"
-#   description = "Allow inbound SSH traffic"
+resource "aws_instance" "ec2_instance" {
+  ami = "ami-0f5f922f781854672"
+  instance_type = "t3.micro"
+  key_name = aws_key_pair.ticket_overflow_key_pair.key_name
+  
+  
+  tags = {
+    Name = "TicketOverflow"
+  }
 
-#   ingress {
-#     from_port = 22
-#     to_port   = 22
-#     protocol  = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     from_port = 80
-#     to_port = 80
-#     protocol = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-# # EC2 Key Pair
-# resource "aws_key_pair" "ticket_overflow_key_pair" {
-#   key_name = "ticket_overflow_key_pair"
-#   public_key = file("./auth/ticket_overflow_key_pair.pub")
-# }
-
-# resource "aws_instance" "ec2_instance" {
-#   ami           = "ami-0f5f922f781854672"
-#   instance_type = "t3.micro"
-#   key_name      = aws_key_pair.ticket_overflow_key_pair.key_name
-
-#     tags = {
-#       Name = "TicketOverflow"
-#     }
-
-#     vpc_security_group_ids = [aws_security_group.ticketoverflow_security.id]
-
-#   # user_data = <<-EOF
-#   #             #!/bin/bash
-#   #             sudo yum update -y
-#   #             sudo yum install docker git
-#   #             sudo service docker start
-#   #             sudo usermod -a -G docker ec2-user
-#   #             sudo chmod 666 /var/run/docker.sock
-#   #             sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-#   #             sudo chmod +x /usr/local/bin/docker-compose
-#   #             git clone https://github.com/Abishtu/CSSE6400-Ticket-Overflow.git
-#   #             cd ~/CSSE6400-Ticket-Overflow
-#   #             docker-compose up --build
-#   #             EOF
-
-#   # Use the file provisioner to copy your source files and Docker files
-# }
+  vpc_security_group_ids = [ aws_security_group.ticket_overflow_security_group.id ]
+  user_data = file("./app_install.sh")
+}
