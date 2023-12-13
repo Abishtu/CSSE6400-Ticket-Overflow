a1cb458fb2c880a62a2120923f7b17d878631f65
Harikesha Suresh <harikeshasuresh@gmail.com>
2023-12-13 12:23:24 +1000

    got db connections sorted, now working on writing user data to it #1

--- ticket_overflow/views/user_service_routes.py
+++ ticket_overflow/views/user_service_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from ticket_overflow import ddb
+import json
 
 api = Blueprint('userAPI', __name__, url_prefix='/api/v1')
 table = ddb.Table('Users')
@@ -11,6 +12,12 @@ def health():
 @api.route('/users', methods=['GET'])
 def get_users():
     items = table.scan()['Items']
+    if len(items) == 0:
+        with open("./ticket_overflow/users.json", "r") as dummyUsersFile:
+                users = json.loads(dummyUsersFile.read())
+                with table.batch_writer() as batch:
+                        for user in users:
+                                batch.put_item(Item=user)
     return jsonify(items), 200
 
 @api.route('/users/<id>', methods=['GET'])
@@ -23,4 +30,9 @@ def get_user(id):
     try:
         return jsonify(response["Item"]), 200
     except KeyError as ke:
+        with open("./ticket_overflow/users.json", "r") as dummyUsersFile:
+                users = json.loads(dummyUsersFile.read())
+                with table.batch_writer() as batch:
+                        for user in users:
+                                batch.put_item(Item=user)
         return jsonify({"error": "user entry does not exist"}), 404
